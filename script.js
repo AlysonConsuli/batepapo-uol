9ade377ed7893c73008f5286f7311b9a15fc2495
Alyson <consulialyson@gmail.com>
2022-02-09 22:02:07 -0300

    selecionar pessoa e mandar mensagem reservada

--- script.js
+++ script.js
@@ -13,49 +13,73 @@ function scroll() {
 
 function entrarNaSala() {
     nomeUsuario()
+    //if (nome !== '') {
     entrada.classList.add('escondido')
     main.innerHTML += `
-    <div class="msg entrouSaiu">
-        <span><small>(09:00:00)</small> <strong>${nome}</strong>  entra na sala...</span>
-    </div>`
+        <div class="msg entrouSaiu">
+            <span><small>(09:00:00)</small> <strong>${nome}</strong>  entra na sala...</span>
+        </div>`
+    jaVerificado = true
     scroll()
+    /*}else{
+        alert('Insira um nome')
+    }*/
 }
 
-let mensagem
+let contato = null
 
 function enviarMsg() {
-    mensagem = document.querySelector('footer input').value
-    main.innerHTML += `
-    <div class="msg" data-identifier="message">
-        <span><small>(09:00:00)</small> <strong>${nome}</strong>  para <strong>Todos</strong>:  ${mensagem}</span>
-    </div>`
+    const mensagem = document.querySelector('footer input').value
+    const input = document.querySelector('footer input')
+
+    let selecionado = document.querySelector('.selecionado')
+    contato = selecionado.parentNode.querySelector('p')
+
+    let visibilidade = document.querySelector('.visibilidade')
+    let selecionadoVisibilidade = visibilidade.querySelector('.selecionado')
+    let seguranca = selecionadoVisibilidade.parentNode.querySelector('p')
+
+    if (seguranca.innerHTML === 'Público') {
+        main.innerHTML += `
+        <div class="msg" data-identifier="message">
+            <span><small>(09:00:00)</small> <strong>${nome}</strong>  para <strong>${contato.innerHTML}</strong>:  ${mensagem}</span>
+        </div>`
+    } else if (seguranca.innerHTML === 'Reservadamente') {
+        main.innerHTML += `
+        <div class="msg privado">
+            <span><small>(09:00:00)</small> <strong>${nome}</strong>  reservadamente para <strong>${contato.innerHTML}</strong>: ${mensagem}</span>
+        </div>`
+    }
+    input.value = ''
     scroll()
+
 }
 
 const telaLateral = document.querySelector('.telaLateral')
 const fundoEscuro = document.querySelector('.escurecer')
 
-function abrirTelaLateral(){
+function abrirTelaLateral() {
     telaLateral.classList.remove('escondido')
-    //telaLateral.classList.add('deslizar')
     fundoEscuro.classList.remove('escondido')
+    //telaLateral.classList.add('deslizar')
 }
 
-function esconderTelaLateral(){
+function esconderTelaLateral() {
     telaLateral.classList.add('escondido')
-    //telaLateral.classList.remove('deslizar')
     fundoEscuro.classList.add('escondido')
+    //telaLateral.classList.remove('deslizar')
 }
 
 
-function selecionar(selecionado, classe){
+function selecionar(selecionado, classe) {
     const check = selecionado.querySelector('.check')
     const classeSelecionado = document.querySelector(`.${classe}`)
+    
     const jaSelecionado = classeSelecionado.querySelector('.selecionado')
-    if(jaSelecionado !== null){
+    if (jaSelecionado !== null) {
         jaSelecionado.classList.remove('selecionado')
     }
-    check.classList.add('selecionado')  
+    check.classList.add('selecionado')
 }
 
 
@@ -92,7 +116,3 @@ document.addEventListener("keypress", function (e) {
     scroll()
 }*/
 
-/*const participantes = document.querySelectorAll('.participante')
-    for(let i = 0; i < participantes.length; i++){
-        participantes[i].classList.add('escondido')
-    }*/
